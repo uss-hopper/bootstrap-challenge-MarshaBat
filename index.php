514ae9459343e44b2961a224cb964944e4a8f565
Marsha Battee <marsha@sanesuite.com>
2019-07-31 22:35:10 -0600

    added index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>BOOTSTRAP CHALLENGE - Marsha Battee</title>
+
+	</head>
+	<body>
+	<?php
+		/**Instructions for Bootstrap Challenge found at this link: https://bootcamp-coders.cnm.edu/~web/bootstrap-4-challenge **/
+	?>
+
+
+
+	</body>
+
+</html>
+
+
+
