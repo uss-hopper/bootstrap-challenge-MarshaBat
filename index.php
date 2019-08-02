0157dde49cb440f1338f8e6c6c4213a96b51c331
Marsha Battee <marsha@sanesuite.com>
2019-08-02 14:52:24 -0600

    added JS, jQuery, Popper links to body

--- index.php
+++ index.php
@@ -10,15 +10,20 @@
 
 		<title>BOOTSTRAP CHALLENGE - Marsha Battee</title>
 
+		<!--Instructions for Bootstrap Challenge found at this link: https://bootcamp-coders.cnm.edu/~web/bootstrap-4-challenge -->
+
 	</head>
 	<body>
-	<?php
-		/**Instructions for Bootstrap Challenge found at this link: https://bootcamp-coders.cnm.edu/~web/bootstrap-4-challenge **/
-	?>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
 		<div>
 			NAVIGATION GOES HERE
 		</div>
-
 
 		<div>
 			<h1>Hello World!</h1>
@@ -51,7 +56,7 @@
 
 		<div>
 			<p>This will be a one-lined footer.</p>
-		</div>>
+		</div>
 
 
 	</body>
