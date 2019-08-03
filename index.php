09fb630e996182f34cd5b5534a1956ba112e029d
Marsha Battee <marsha@sanesuite.com>
2019-08-03 09:05:03 -0600

    section 3, without margins fixed

--- index.php
+++ index.php
@@ -25,8 +25,8 @@
 
 		<div class="jumbotron">
 			<div class="container">
-				<div class="row justify-content-sm-center">
-					<div class="col-sm">
+				<div class="row">
+					<div class="col col-sm-10">
 						<h1>Hello, World!</h1>
 						<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 						<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
@@ -35,14 +35,45 @@
 			</div>
 		</div>
 
+
+
+
+
+
+
+
+
+		<div class="container">
+			<div class="row align-items-center">
+
+					<div class="col-sm-4">
+						<img src="sampleimage.JPG" alt="this image will be replaced" width="300">
+					</div>
+
+					<div class="col-sm-8">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+							aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+							aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div>
+
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
 		<div>
-			<img src="sampleimage.JPG" alt="this image will be replaced" width="300">
-			<p>The following are two paragraphs of content.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-				dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-				ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-				nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-				anim id est laborum.</p>
+			<img src="sampleimage.JPG" alt="this is another image to be replaced" width="300">
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
 				dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
 				ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
@@ -60,8 +91,34 @@
 		</div>
 
 		<div>
+			<img src="sampleimage.JPG" alt="this is another image to be replaced" width="300">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+				dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+				ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+				nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+				anim id est laborum.</p>
+		</div>
+
+		<div>
+			<img src="sampleimage.JPG" alt="this is another image to be replaced" width="300">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+				dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+				ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+				nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+				anim id est laborum.</p>
+		</div>
+
+
+
+
+
+		<div>
 			<p>This will be a one-lined footer.</p>
 		</div>
+
+
+
+
 
 
 		<!-- Optional JavaScript -->
